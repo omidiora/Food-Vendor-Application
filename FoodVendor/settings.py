3cd665133ac7b887fa0e3eb4cac619c9182ff1d1
omidiora <omidioraemmanuel@gmail.com>
2020-05-26 17:45:14 +0100

    files changed

--- FoodVendor/settings.py
+++ FoodVendor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o-2^zne%ft=j-=y2(bn*-$!51k0d!#0fukz*g3q)pwd_7f@v#^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://foodvendorapp.herokuapp.com/','']
+ALLOWED_HOSTS = ['https://foodvendorapp.herokuapp.com/','*']
 
 
 # Application definition
